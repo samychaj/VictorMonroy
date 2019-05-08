ad81d18be458e601a3414cf70f9d80f6d3aa5824
samychaj <50414134+samychaj@users.noreply.github.com>
2019-05-08 11:52:52 -0600

    inicial

---
+++ Roles/Clases/Conexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Roles.Clases
+{
+    public class Conexion
+    {
+        SqlConnection conn = new SqlConnection();
+        public SqlConnection Openn()
+        {
+            conn.ConnectionString = "Data Source=localhost;Initial Catalog=prueba;Persist Security Info=True;User ID=sa;Password=<prueba20>";
+            try
+            {
+                conn.Open();
+            }
+            catch(Exception e)
+            {
+                conn.Close();
+            }
+            return conn;
+        }
+
+        public void Closee()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ Roles/Models/RegistroModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Roles.Models
+{
+    public class RegistroModels
+    {
+            public int Iduser { get; set; }
+            public string Username { get; set; }
+            public int Idrole { get; set; }
+
+    }
+}
